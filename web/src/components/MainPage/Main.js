4cbb6f6095bac26e151bf187b375b3958ef926b8
Cat26 <julia.joanna.hardy@gmail.com>
2021-05-29 17:02:25 +0200

    main page

--- web/src/components/MainPage/Main.js
+++ web/src/components/MainPage/Main.js
@@ -1,11 +1,18 @@
 import MenuBar from 'components/MenuBar/MenuBar';
 import React from 'react';
 import style from './MainPage.module.scss';
+import czerwoneOkregi from '../../assets/img/czerwone-okregi.png';
 
 const Main = () => {
     return (
         <div className={style.wrapper}>
             <MenuBar></MenuBar>
+            <div className={style.topImage}>
+                <h1>MEditate</h1>
+                <p>Jeśli zdarzył Ci się kiedyś idealny moment bycia tu i teraz, który nagle przerywał Ci głos odgłos dzwonków lub głos mówiący “jeśli uciekłaś myślami, wróć do swojego oddechu”. To MEditate jest storzone z myślą o Tobie.</p>
+                <button>Zaczynamy!</button>
+            </div>
+            
         </div>
     )
 };
